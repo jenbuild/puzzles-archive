0d76070b26ab4891cc9ae53272b096ce0a2ee762
Ajay Kumar <iajaymk+github@gmail.com>
2026-05-29 23:02:10 +0530

    Add Homepage UI

---
+++ constants/gameList.ts
@@ -0,0 +1,30 @@
+export const gameNames = [
+  {
+    name: "Patches",
+    image: "/images/patches.svg",
+  },
+  {
+    name: "Zip",
+    image: "/images/zip.svg",
+  },
+  {
+    name: "Mini-Sudoku",
+    image: "/images/mini-sudoku.svg",
+  },
+  {
+    name: "Tango",
+    image: "/images/tango.svg",
+  },
+  {
+    name: "Queens",
+    image: "/images/queens.svg",
+  },
+  {
+    name: "Pinpoint",
+    image: "/images/pinpoint.svg",
+  },
+  {
+    name: "Crossclimb",
+    image: "/images/crossclimb.svg",
+  },
+];
